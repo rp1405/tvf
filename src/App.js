f7a4ae9e79ba5c4321583d9550ce5ccbf75fff8f
rp1405 <rpareek140503@gmail.com>
2023-03-31 01:08:31 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Tray from "./components/tray.jsx";
 function App() {
-  return <Tray name="Pizza" price="600" veg={0} />;
+  return <Tray name="Rohan" price="1000" veg={1} />;
 }
 
 export default App;
