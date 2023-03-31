3ac0bdf726d3391ff353dead672ff462b61b3d92
rp1405 <rpareek140503@gmail.com>
2023-03-31 11:07:36 +0530

    major changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 import Tray from "./components/tray.jsx";
 function App() {
-  return <Tray name="Pizza" price="600" veg={0} />;
+  const dummy = [
+    { name: "rohan", price: 1000, veg: 1 },
+    { name: "Pareek", price: 500, veg: 0 },
+    { name: "gems", price: 10, veg: 1 },
+    { name: "potty", price: 14000, veg: 0 },
+    { name: "dosa", price: 4000, veg: 0 },
+    { name: "yuvraj", price: 100, veg: 1 },
+    { name: "rohan", price: "1000", veg: 1 },
+    { name: "rohan", price: "1000", veg: 1 },
+    { name: "rohan", price: "1000", veg: 1 },
+    { name: "rohan", price: "1000", veg: 1 },
+    { name: "rohan", price: "1000", veg: 1 },
+    { name: "rohan", price: "1000", veg: 1 },
+    { name: "rohan", price: "1000", veg: 1 },
+    { name: "tanvi", price: "0", veg: 0 },
+  ];
+  return dummy.map((e) => <Tray name={e.name} price={e.price} veg={e.veg} />);
 }
 
 export default App;
