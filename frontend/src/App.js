3f8d7e2bf1ec0142fca57ec11613971614178dbb
rohanrg18 <89922339+rohanrg18@users.noreply.github.com>
2023-04-01 19:51:11 +0530

    Update App.js
    git comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Header from "./components/header.jsx";
 import { useState, useEffect } from "react";
 import { getFoodsData } from "./api/foodoptions";
 function App() {
+  // git comment
+
   const [isCheck, setIsCheck] = useState(0);
   const [countArr, setCountArr] = useState([
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
