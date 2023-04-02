dd08335711c0a36c512af1695ca7fce8519129d2
rohanrg18 <89922339+rohanrg18@users.noreply.github.com>
2023-04-03 03:47:58 +0530

    verified order status update functionality

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -5,6 +5,22 @@ const getAllTrueOrder = async (req, res) => {
   res.status(200).json({ output });
 };
 
+const updateOrder = async (req, res) => {
+  const { id: taskID } = req.params;
+
+  const task = await VerifiedOrder.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404));
+  }
+
+  res.status(200).json({ task });
+};
+
 module.exports = {
   getAllTrueOrder,
+  updateOrder,
 };
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllTrueOrder } = require("../controllers/order.js");
+const { getAllTrueOrder, updateOrder } = require("../controllers/order.js");
 
 router.route("/true").get(getAllTrueOrder);
+router.route("/:id").patch(updateOrder);
 
 module.exports = router;
